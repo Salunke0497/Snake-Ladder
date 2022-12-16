19c453d4dc405a3c549a79f2d9ea4f05de002370
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-12-16 17:42:40 +0530

    welcome

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to snake and ladder");
+            int Palyer1 = 0;
+            Console.WriteLine("Starts with single player position : " + 0);
+        }
+    }
+}
