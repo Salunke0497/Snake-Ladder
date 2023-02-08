366204d3714e38a6b2a7376a7ef03030eb7b1913
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2023-02-08 19:22:11 +0530

    welcome

---
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome Game Snake-Ladder");
+            int Position = 0;
+            Console.WriteLine("Your position is : " + Position);
+
+        }
+    }
+}
